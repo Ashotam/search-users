13bf1be374763686535306a778bee73f658ce3d5
ashotam <amiraghyan.ashot@gmail.com>
2024-07-17 22:32:21 +0400

    fixed error handling

--- user-search-backend/src/app.ts
+++ user-search-backend/src/app.ts
@@ -10,9 +10,13 @@ const port = config.PORT || 5000;
 const uri= config.MONGODB_URI||""
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use('/api', indexRouter);
-mongoose.connect(uri).then(() => {
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  useCreateIndex: true,      
+  useFindAndModify: false
+}).then(() => {
     console.log("MongoDB connected successfully");
   }).catch((err) => {
     console.error("MongoDB connection error:", err);
--- user-search-frontend/src/App.tsx
+++ user-search-frontend/src/App.tsx
@@ -10,26 +10,39 @@ const App: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [cancelTokenSource, setCancelTokenSource] = useState<ReturnType<typeof axios.CancelToken.source>>();
 
   const handleUserFormSubmitSuccess = async (formData: User) => {
-    setLoading(true);
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('Operation canceled due to new request.');
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    setLoading(true); 
+
     try {
-      const response = await axios.post<User[]>(`${baseUrl}/api/search`, formData);
+      const response = await axios.post<User[]>(`${baseUrl}/api/search`, formData, {
+        cancelToken: source.token,
+      });
       setUsers(response.data);
+      setLoading(false)
       setError(null);
     } catch (error) {
-      console.error('Search request failed:', error);
+      if (!axios.isCancel(error)) {
+        console.error('Search request failed:', error);
+      }
       setError('Failed to search users. Please try again.');
-    } finally {
-      setLoading(false);
-    }
+    } 
   };
+
   const resetForm = () => {
     setUsers([]);
   };
+
   return (
     <div className="app-container">
-      <UserForm onSubmitSuccess={handleUserFormSubmitSuccess}  resetForm={resetForm} />
+      <UserForm onSubmitSuccess={handleUserFormSubmitSuccess} resetForm={resetForm} />
       {loading && <p>Loading...</p>}
       {error && <p className="error-message">{error}</p>}
       <div className="card-list">
